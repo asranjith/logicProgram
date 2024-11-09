67068a0f0ba58c3436f147a2439eac66b3d2d341
asranjith <rockyranjith27@gmail.com>
2024-11-09 10:29:57 +0530

    program push

---
+++ src/practiceArray/OccuranceOfAnElement.java
@@ -0,0 +1,26 @@
+package practiceArray;
+
+public class OccuranceOfAnElement {
+    public static void main(String[] args) {
+        int [] nums={1,5,3,6,7,6,3,4,1,3,9};
+        int [] arr=new int[nums.length];
+        int visited=-1;
+        for(int i=0;i<nums.length;i++){
+            int count=1;
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                    arr[j]=visited;
+                }
+            }
+            if(arr[i]!=visited){
+                arr[i]=count;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+        if(arr[i]!=visited){
+            System.out.println(nums[i]+"====>"+arr[i]);
+        }
+        }
+    }
+}
